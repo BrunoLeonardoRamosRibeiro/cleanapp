4a4b9f9c3f9b53e91e240ccb85f469d73d5e7492
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2020-11-28 09:55:34 -0300

    Aula 01 - Views

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,26 @@
+import 'package:cleanapp/app/controllers/app_controller.dart';
+import 'package:cleanapp/app/pages/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: AppController.instance.themeSwitch,
+      builder: (context, isDark, child)  {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            brightness: isDark?Brightness.dark : Brightness.light,
+          ),
+          home: HomePage(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/app/controllers/app_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppController {
+  static final AppController instance = AppController._();
+  AppController._();
+
+  final themeSwitch = ValueNotifier<bool>(false);
+
+  changeTheme(bool value){
+    themeSwitch.value = value;
+  }
+
+
+}
