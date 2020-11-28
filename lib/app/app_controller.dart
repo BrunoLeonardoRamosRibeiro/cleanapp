0142a21abacd7020820157ae04bc76e3422afc41
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2020-11-28 16:41:01 -0300

    Aula 04 - ViewModels

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,28 +1,22 @@
 import 'package:cleanapp/app/interfaces/local_storage_interface.dart';
 import 'package:cleanapp/app/models/appconfig_model.dart';
 import 'package:cleanapp/app/services/shared_local_storage_service.dart';
+import 'package:cleanapp/app/viewmodels/change_theme_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class AppController {
   static final AppController instance = AppController._();
-  AppController._(){
-    storage.get('isDark').then((value){
-      if (value != null)
-      config.themeSwitch.value = value;
-    });
+
+  AppController._() {
+    changeThemeViewModel.init();
   }
 
-  final AppConfigModel config = AppConfigModel();
+  final ChangeThemeViewModel changeThemeViewModel =
+      ChangeThemeViewModel(storage: SharedLocalStorageService());
 
-  bool get isDark => config.themeSwitch.value;
-  ValueNotifier<bool> get themeSwitch => config.themeSwitch;
+  bool get isDark => changeThemeViewModel.config.themeSwitch.value;
 
-  final ILocalStorage storage = SharedLocalStorageService();
-
-  changeTheme(bool value){
-    config.themeSwitch.value = value;
-    storage.put('isDark', value);
-  }
-
+  ValueNotifier<bool> get themeSwitch =>
+      changeThemeViewModel.config.themeSwitch;
 
 }
